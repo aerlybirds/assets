36ee9281e49f6d24e5c23fbaf7d7fa6aabb76895
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2025-03-16 10:06:16 +1000

    Create texts_pics.py

---
+++ verify/texts_pics.py
@@ -0,0 +1,12 @@
+pictures = {
+    "Main": "",
+    "Welcome": "",
+    "Access denied": "",
+    "Access granted": "",
+    "Connect": "",
+    "Welcome to chat": "",
+    "TOP holders": "",
+    "TOP select token": "",
+    "Balance command": "",
+    "Balance command no tokens": "",
+}
